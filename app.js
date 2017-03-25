02b4a8e223a9af07280064c033c860975740a2a3
Marco Albarelli <info@marcoalbarelli.eu>
2017-03-25 13:22:05 +0100

    Default express app created Socket setup WriteStream

---
+++ app.js
@@ -0,0 +1,55 @@
+'use strict'
+const express = require('express')
+const app = express();
+const expressWs = require('express-ws')(app)
+
+const path = require('path')
+const favicon = require('serve-favicon')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const bodyParser = require('body-parser')
+
+const ws = require('./routes/ws')
+const index = require('./routes/index')
+const users = require('./routes/users')
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'twig')
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', index)
+app.use('/users', users)
+app.use('/ws', ws)
+
+
+const ReadActionsStream = require('./streams/readActionsStream')
+app.ras = new ReadActionsStream({wss: expressWs.getWss()})
+// app.use('/ws', ws)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
