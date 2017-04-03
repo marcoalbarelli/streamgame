552b5ee26e8fc98fb1ab7d1df6e4b06067032cfe
Marco Albarelli <info@marcoalbarelli.eu>
2017-04-03 09:12:14 +0200

    WIP streams in and out

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'twig')
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'public/images', 'bevr_logo_tentative.png')))
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -32,7 +32,6 @@ app.use('/ws', ws)
 
 const ReadActionsStream = require('./streams/readActionsStream')
 app.ras = new ReadActionsStream({wss: expressWs.getWss()})
-// app.use('/ws', ws)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
