4a9a6290fa5e600aa48103612196c3a25c809faa
Marco Albarelli <info@adhocmobile.it>
2017-05-01 00:09:34 +0200

    glassFilledEvent

--- routes/ws.js
+++ routes/ws.js
@@ -8,7 +8,7 @@ const router = express.Router();
 /* GET home page. */
 router.ws('/echo', function(ws, req) {
     ws.on('message', function(msg) {
-        req.app.ras.write('something has indded happened')
+        req.app.ras.write(msg)
         // ws.send(msg);
     })
 });
--- streams/readActionsStream.js
+++ streams/readActionsStream.js
@@ -10,19 +10,53 @@ class ReadActionStream extends Writable {
         if(options.wss){
             this._wss = options.wss
         }
+        this.MAX_CAPACITY = 2180
         this._currentAmount = 0
     }
 
     write(chunk) {
-        this._currentAmount++
+        if(chunk == 'leak'){
+            this.simulateLeak()
+        } else  {
+            this._currentAmount++
+        }
+
+        if(this.checkIfGlassIsFull()) {
+            this.sendGlassHasBeenFilled()
+        } else {
+            this.sendLevel()
+        }
+    }
+
+    checkIfGlassIsFull() {
+        if(this._currentAmount >= this.MAX_CAPACITY) {
+            return true
+        }
+        return false
+    }
+
+    sendLevel() {
         const self = this
         if(this._currentAmount % 10 == 0){
             this._wss.clients.forEach(function each(client) {
                 if (client.readyState === WebSocket.OPEN) {
-                    client.send(self._currentAmount);
+                    client.send(self._currentAmount)
                 }
-            });
+            })
         }
+    }
+
+    sendGlassHasBeenFilled() {
+        const self = this
+        this._wss.clients.forEach(function each(client) {
+            if (client.readyState === WebSocket.OPEN) {
+                client.send('glassHasBeenFilled')
+            }
+        })
+    }
+
+    simulateLeak() {
+        this._currentAmount = 0
     }
 
     getCurrentAmount(){
